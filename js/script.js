6dec203aa0077803cb688aaca89d72a4d9e10054
Bartosz Kolsut <locagus@o2.pl>
2020-06-10 17:00:38 +0200

    active button in gallery

--- js/script.js
+++ js/script.js
@@ -64,3 +64,15 @@ navBar.addEventListener('click', function () {
     navBar.classList.toggle('active')
     mobileMenu.classList.toggle('active')
 })
+
+
+
+//Open gallery
+
+const sectionOneButton = document.querySelector('div.main-wrapper button')
+const sectionOneGallery = document.querySelector('div.gallery-wrapper')
+
+sectionOneButton.addEventListener('click', function () {
+    sectionOneButton.classList.toggle('active')
+    sectionOneGallery.classList.toggle('active')
+})
